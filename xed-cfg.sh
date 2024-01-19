6af42acca2e68a891203a4e40213db6cf2fedbb0
Liz <31071746+caltrop2075@users.noreply.github.com>
2024-01-19 04:41:19 -0500

    Add files via upload

---
+++ xed-cfg.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+clear
+source ~/data/global.dat
+
+t0="$HOME/Documents/Linux/Configuration/xed"
+t1="/usr/share/gtksourceview-3.0/styles"
+t2="/usr/share/gtksourceview-4/styles"
+
+micro "$t0/custom.xml"
+cp -v "$t0/custom.xml" "$t1"
+cp -v "$t0/custom.xml" "$t2"
